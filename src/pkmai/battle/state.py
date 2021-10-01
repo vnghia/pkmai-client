47dba428e4301a37d519ebb19fd5e5e6719dd43e
Võ Văn Nghĩa <vovannghia2409@gmail.com>
2021-10-01 23:54:21 +0200

    battle(state): fix typo

--- src/pkmai/battle/state.py
+++ src/pkmai/battle/state.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 from pkmai.battle.pokemon import Pokemon
 
@@ -8,21 +8,33 @@ from pkmai.battle.pokemon import Pokemon
 class State:
     def __init__(
         self,
-        pokemons: Dict[str, Pokemon] = None,
-        opponents: Dict[str, Pokemon] = None,
+        pokemons: List[Pokemon] = None,
+        active_pokemons: Dict[Tuple[str, str, str], Pokemon] = None,
+        opponents: List[Pokemon] = None,
+        active_opponents: Dict[Tuple[str, str, str], Pokemon] = None,
         positions: List[str] = None,
     ) -> None:
-        self._pokemons = pokemons or {}
-        self._opponents = opponents or {}
+        self._pokemons = pokemons or []
+        self._active_pokemons = active_pokemons or {}
+        self._opponents = opponents or []
+        self._active_opponents = active_opponents or {}
         self._positions = positions or []
 
     @property
-    def pokemons(self) -> Dict[str, Pokemon]:
+    def pokemons(self) -> List[Pokemon]:
         return self._pokemons
 
     @property
-    def opponents(self) -> Dict[str, Pokemon]:
+    def active_pokemons(self) -> Dict[Tuple[str, str, str], Pokemon]:
+        return self._active_pokemons
+
+    @property
+    def opponents(self) -> List[Pokemon]:
         return self._opponents
+
+    @property
+    def active_opponents(self) -> Dict[Tuple[str, str, str], Pokemon]:
+        return self._active_opponents
 
     @property
     def positions(self) -> List[str]:
