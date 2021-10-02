3d41965182766b0c118eee1b959a146c9b4f51ab
Võ Văn Nghĩa <vovannghia2409@gmail.com>
2021-10-02 12:04:32 +0200

    battle(move): use moveset and add move database

--- src/pkmai/battle/db.py
+++ src/pkmai/battle/db.py
@@ -13,3 +13,17 @@ class MoveDB:
         "https://play.pokemonshowdown.com/data/moves.json"
     )
     db_name: Dict = {move["name"]: dict(id=id, **move) for id, move in db_id.items()}
+
+    @classmethod
+    def to_id(cls, name_or_id: str) -> str:
+        if name_or_id in cls.db_id:
+            return name_or_id
+        else:
+            return cls.db_name[name_or_id]["id"]
+
+    @classmethod
+    def to_name(cls, name_or_id: str) -> str:
+        if name_or_id in cls.db_name:
+            return name_or_id
+        else:
+            return cls.db_id[name_or_id]["name"]
--- src/pkmai/battle/move.py
+++ src/pkmai/battle/move.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
-from typing import Dict, Literal
+from typing import Dict, List, Literal
+
+from pkmai.battle.db import MoveDB
 
 
 class Move:
@@ -26,7 +28,7 @@ class Move:
             "scripted",
             "self",
         ] = "normal",
-        type: Literal["", "mega", "zmove", "max"] = "",
+        type: Literal["", "zmove", "max"] = "",
     ) -> None:
         self._name = name
         self._id = id
@@ -34,6 +36,8 @@ class Move:
         self._maxpp = maxpp
         self._target = target
         self._type = type
+        if self._type == "zmove":
+            self._maxpp = 1
         self._disable = False
 
     @property
@@ -79,7 +83,7 @@ class Move:
         return self._target
 
     @property
-    def type(self) -> Literal["", "mega", "zmove", "max"]:
+    def type(self) -> Literal["", "zmove", "max"]:
         return self._type
 
     @property
@@ -113,3 +117,138 @@ class Move:
             self._target = move_dict["target"]
         self._pp = int(move_dict["pp"])
         self._disable = "disable" in move_dict
+
+
+class MoveSet:
+    def __init__(
+        self,
+        moves: List[Move] = None,
+        max_moves: List[Move] = None,
+        zmoves: List[Move] = None,
+        can_max: bool = False,
+        can_z: bool = False,
+        max: bool = False,
+    ):
+        self._moves = moves or []
+        self._max_moves = max_moves or []
+        self._zmoves = zmoves or []
+        self._moves_name: Dict[str, int] = {
+            move.name: idx for idx, move in enumerate(self._moves)
+        }
+        self._max_moves_name: Dict[str, int] = {
+            move.name: idx for idx, move in enumerate(self._max_moves)
+        }
+        self._zmoves_name: Dict[str, int] = {
+            move.name: idx for idx, move in enumerate(self._zmoves)
+        }
+        self._can_max = can_max
+        self._can_z = can_z
+        self._max = max
+
+    @property
+    def current(self) -> List[Move]:
+        current_set = self.moves if not self.max else self.max_moves
+        return current_set + self.zmoves
+
+    @property
+    def moves(self) -> List[Move]:
+        return self._moves
+
+    @property
+    def max_moves(self) -> List[Move]:
+        if not self.can_max:
+            return []
+        return self._max_moves
+
+    @property
+    def zmoves(self) -> List[Move]:
+        if not self.can_z:
+            return []
+        return self._zmoves
+
+    @property
+    def moves_name(self) -> Dict[str, int]:
+        return self._moves_name
+
+    @property
+    def max_moves_name(self) -> Dict[str, int]:
+        if not self.can_max:
+            return {}
+        return self._max_moves_name
+
+    @property
+    def zmoves_name(self) -> Dict[str, int]:
+        if not self.can_z:
+            return {}
+        return self._zmoves_name
+
+    @property
+    def can_max(self) -> bool:
+        return self._can_max
+
+    @can_max.setter
+    def can_max(self, can_max: bool):
+        self._can_max = can_max
+
+    @property
+    def can_z(self) -> bool:
+        return self._can_z
+
+    @can_z.setter
+    def can_z(self, can_z: bool):
+        self._can_z = can_z
+
+    @property
+    def max(self) -> bool:
+        return self._max
+
+    @max.setter
+    def max(self, max: bool):
+        if not self._can_max:
+            raise ValueError("Can not max")
+        else:
+            self._max = max
+
+    # ---------------------------------- String ---------------------------------- #
+
+    def __repr__(self) -> str:
+        return f"({hex(id(self))}) {self}"
+
+    def __str__(self) -> str:
+        return "; ".join(map(str, self.moves))
+
+    # ----------------------------------- Copy ----------------------------------- #
+
+    def clone(self) -> MoveSet:
+        clone = MoveSet()
+        clone._moves = [move.clone() for move in self._moves]
+        clone._max_moves = [move.clone() for move in self._max_moves]
+        clone._zmoves = [move.clone() for move in self._zmoves]
+        clone._moves_name = dict(**self._moves_name)
+        clone._max_moves_name = dict(**self._max_moves_name)
+        clone._zmoves_name = dict(**self._zmoves_name)
+        clone._can_max = self._can_max
+        clone._can_z = self._can_z
+        return clone
+
+    def add(self, name):
+        if name in self.moves_name:
+            self.moves[self.moves_name[name]].pp -= 1
+        elif name in self.max_moves_name:
+            self.max_moves[self.max_moves_name[name]].pp -= 1
+        elif name in self.zmoves_name:
+            self.zmoves[self.zmoves_name[name]].pp -= 1
+            self.can_z = False
+        move_dict: Dict = MoveDB.db_name.get[name]
+        move = Move(move_dict["id"], name, move_dict["pp"], move_dict["target"])
+        if "isMax" in move_dict:
+            move._type = "max"
+            self.max_moves.append(move)
+            self.max_moves_name[move.name] = len(self.max_moves) - 1
+        elif "isZ" in move_dict:
+            move._type = "zmove"
+            self.zmoves.append(move)
+            self.zmoves_name[move.name] = len(self.zmoves) - 1
+        else:
+            self.moves.append(move)
+            self.moves_name[move.name] = len(self.moves) - 1
